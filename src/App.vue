5ad28b4bab8d2647c30521c3b5a14fc5e7bdbf6c
ultraman-agul <agulagul@163.com>
2022-03-03 09:51:11 +0800

    fix：修复使用别名时vscode报错

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  width: 100%;
+  height: 100%;
 }
 </style>
