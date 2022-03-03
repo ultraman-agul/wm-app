d800c404436daac68a8a36eaaa2c638155a6f98d
ultraman-agul <agulagul@163.com>
2022-03-03 13:03:52 +0800

    feat: 顶部topHead

---
+++ src/components/TopHead.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="head">
+        <span class="iconfont icon-arrow-left-bold go-back"></span>
+        <span class="title">{{title}}</span>
+        <span class="more iconfont icon-elipsis" v-if="more"></span>
+    </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+    title: string,
+    more: boolean
+    }>()
+</script>
+
+<style lang="scss">
+.head{
+    position: relative;
+    background-color: yellow;
+    text-align: center;
+    line-height: 1rem;
+    padding: 0 0.2rem;
+    .go-back,
+    .more {
+        position: absolute;
+        i {
+        font-size: 0.7rem;
+        }
+    }
+    .go-back {
+        left: 10px;
+    }
+    .title {
+        flex: 1;
+        font-size: 0.5rem;
+        font-weight: bold;
+        text-align: center;
+    }
+    .more {
+        right: 15px;
+    }
+}
+</style>
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,7 +1,9 @@
 <template>
+    <top-head title="美团外卖"></top-head>
     <tab-bar></tab-bar>
 </template>
 
 <script lang="ts" setup>
 import TabBar from "@/components/TabBar.vue";
+import TopHead from "@/components/TopHead.vue";
 </script>
