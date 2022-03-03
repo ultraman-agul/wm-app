4c024c936772a231ecf6b813cecf2a64f001073f
ultraman-agul <agulagul@163.com>
2022-03-03 22:33:31 +0800

    feat: 改用postcss-px-to-viewport适配

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -2,18 +2,21 @@
     <div class="index" ref="scrollWrapper">
         <div class="main-container">
             <!-- 头部 -->
-            <v-head title="美团外卖"></v-head>
-            <var-button type="primary">主要按钮</var-button>
-            <var-button type="success">成功按钮</var-button>
             <div class="guide-container">
                 <router-link to="/location?fromIndex=true" class="location">
-                    <i class="iconfont icon-map"></i>
-                    <span class="address">{{}}</span>
-                    <i class="iconfont">&#xe6d7;</i>
+                    <var-icon name="map-marker" />
+                    <span class="address">{{"广州南"}}</span>
+                    <var-icon name="chevron-right" />
                 </router-link>
                 <router-link to="/search" class="search">
-                    <i class="iconfont icon-search"></i>
-                    <span>请输入商家、商品名</span>
+                    <div class="left-content">
+                        <var-icon name="magnify" />
+                        <span>请输入商家或商品名称</span>
+                    </div>
+
+                    <var-button type="primary" class="search-btn">
+                        搜索
+                    </var-button>
                 </router-link>
             </div>
             <!--导航轮播部分-->
@@ -36,3 +39,49 @@
 </template>
 
 <script lang="ts" setup></script>
+
+<style lang="scss">
+.index{
+    .main-container{
+        font-size: 16px;
+        .guide-container{
+            padding: 20px 10px;
+            background-color: $mtYellow;
+            .location{
+                display: flex;
+                align-items: center;
+                .address{
+                    font-weight: bolder;
+                }
+            }
+            .search{
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                background-color: #ffffff;
+                height: 40px;
+                margin: 10px 0;
+                border-radius: 20px;
+                padding: 20px;
+                color: #999999;
+                .left-content{
+                    display: flex;
+                    align-items: center;
+                    span{
+                        margin-left: 5px;
+                    }
+                }
+
+                .search-btn{
+                    height: 30px;
+                    width: 60px;
+                    top: -1px;
+                    border-radius: 35px;
+                    margin-right: -15px;
+                    color: #000;
+                }
+            }
+        }
+    }
+}
+</style>
