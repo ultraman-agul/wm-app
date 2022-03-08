91ccc81cdb90ff4f52ccbbaffa45b92485210c4c
ultraman-agul <agulagul@163.com>
2022-03-08 12:16:23 +0800

    feat: 搜索结果的展示

--- src/views/location/index.vue
+++ src/views/location/index.vue
@@ -9,35 +9,58 @@
             </var-input>
             <var-button type="primary">搜索</var-button>
         </div>
-
-        <var-button block @click="getNowLocation"><var-icon name="map-marker-radius" />点击定位当前位置</var-button>
+        <ul>
+            <li v-for="item in state.addressList" :key="item.id">
+                <p>{{ item.title }}</p>
+                <p>{{ item.address }}</p>
+            </li>
+        </ul>
+        <var-button block color="#fff" @click="getNowLocation"><var-icon name="map-marker-radius" />点击定位当前位置</var-button>
     </div>
 </template>
 
 <script setup lang="ts">
 import { useAddressStore } from '@/store/address'
 import { locationSearch } from '@/api/location'
-import { debounce } from '@/utils/tools'
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
+import { Snackbar } from '@varlet/ui'
 
 let address = ref('')
+let timer: any
+const state = reactive({
+    addressList: []
+})
+
 const store = useAddressStore()
 
 function getNowLocation() {
     store.getNowlocation()
 }
 
-const debounceSearch = debounce(locationSearch, 1000) // 这个函数起到防抖的作用，短时间多次触发，只执行最后一次
 function search(val:any) {
-    console.log(111)
-    debounceSearch(({ keyword: val }))
+    if (timer) {
+        clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+        locationSearch({ keyword: val }).then((data:any) => {
+            if (data.status === 200) {
+                state.addressList = data.data.data
+            } else {
+                Snackbar.error('获取位置失败')
+            }
+        }).catch((e: string) => {
+            Snackbar.error('获取位置失败' + e)
+        })
+    }, 1000)
 }
 </script>
 
 <style lang="scss" scoped>
 .location {
+    background-color: #f4f4f4;
+    height: 100%;
+
     button {
-        margin-top: 20px;
         padding: 30px 0;
     }
 
@@ -45,13 +68,30 @@ function search(val:any) {
         display: flex;
         justify-content: space-between;
         align-items: center;
-        padding: 0 10px;
+        padding: 20px 10px 10px;
+        background-color: #fff;
 
         button {
             margin: 0;
             width: 100px;
             padding: 10px;
             margin-left: 10px;
+        }
+    }
+
+    ul {
+        background-color: #fff;
+        padding: 10px;
+
+        li {
+            padding: 10px;
+            border-bottom: 1px solid #f4f4f4;
+
+            p:nth-child(2) {
+                padding-top: 4px;
+                color: #aaa;
+                font-size: 14px;
+            }
         }
     }
 }
